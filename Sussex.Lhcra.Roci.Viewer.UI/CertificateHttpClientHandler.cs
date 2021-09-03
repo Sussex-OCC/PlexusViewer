e4f03747afd24c255a4101d3ba0d715a6a8288f7
Bruno Ohiro <cabbashlive@gmail.com>
2021-09-03 02:30:37 +0100

    added using azure cert

--- Sussex.Lhcra.Roci.Viewer.UI/CertificateHttpClientHandler.cs
+++ Sussex.Lhcra.Roci.Viewer.UI/CertificateHttpClientHandler.cs
@@ -21,7 +21,7 @@ namespace Sussex.Lhcra.Roci.Viewer.UI
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var certificate = await _appSecretsProvider.GetCertificate(_clientCerConfig.LocalCertificateSerialNumber);
+            var certificate = await _appSecretsProvider.GetCertificate(_clientCerConfig.KeyVaultCertificateName);
             request.Headers.Add("X-ClientCert",Convert.ToBase64String(certificate.GetRawCertData()));
             return await base.SendAsync(request, cancellationToken);
         }
