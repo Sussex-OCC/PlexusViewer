0fa5ade0d5e341df17c116d601a7278e5d78c03b
Bruno Ohiro <cabbashlive@gmail.com>
2021-09-28 01:51:09 +0100

    Added Globalisation To Age Calc

--- Sussex.Lhcra.Roci.Viewer.UI/Extensions/SessionExtensions.cs
+++ Sussex.Lhcra.Roci.Viewer.UI/Extensions/SessionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Globalization;
 using System.Text.Json;
 
 namespace Sussex.Lhcra.Roci.Viewer.UI.Extensions
@@ -8,10 +9,15 @@ namespace Sussex.Lhcra.Roci.Viewer.UI.Extensions
     {
         public static int CalculateAge(this DateTime dateOfBirth)
         {
-            DateTime Now = DateTime.Now;
-            int years = new DateTime(DateTime.Now.Subtract(dateOfBirth).Ticks).Year - 1;
-            return years;
+            dateOfBirth = DateTime.ParseExact(dateOfBirth.ToString(), "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
+            var now = DateTime.Now;
+
+            now = DateTime.ParseExact(now.ToString(), "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+            int years = new DateTime(DateTime.Now.Subtract(dateOfBirth).Ticks).Year - 1;
+
+            return years;
         }
         public static void Set<T>(this ISession session, string key, T value)
         {
